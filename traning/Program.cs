0475bdc802c391cb9fd274e5e7a10e116a6fbf50
pc <pc@DESKTOP-R937SDA>
2024-02-07 19:49:22 +0300

    Add project files.

---
+++ traning/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+
+#region "train 1" 
+//int sayi1 = 0, sayi2 = 2;
+
+//int toplam = (sayi1 + sayi2);
+//Console.WriteLine(toplam);
+//Console.Read();
+
+
+#endregion
+
+
+
+#region "train 2"
+//Console.WriteLine("lütfen ilk sayıyı giriniz ? ");
+//int sayi1 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("lütfen ikinci sayıyı giriniz? ");
+//int sayi2 = Convert.ToInt32(Console.ReadLine());
+
+//int toplam = (sayi1 + sayi2);
+
+//Console.WriteLine("sayıların toplamı" + " \t " + toplam);
+
+
+
+#endregion
+
+
+
+
+
